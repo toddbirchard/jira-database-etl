975d466078ff8b9fc553bf54a997a42abff98f2a
Todd Birchard <toddbirchard@gmail.com>
2019-03-22 23:31:52 -0400

    moved tables to env, removed index error

--- jira_etl/db.py
+++ jira_etl/db.py
@@ -12,9 +12,9 @@ class DatabaseImport:
     """Merge Epic metadata and upload JIRA issues."""
 
     URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    epic_table = os.environ.get('SQLALCHEMY_EPIC_TABLE')
-    jira_table = os.environ.get('SQLALCHEMY_JIRA_TABLE')
-    db_name = 'hackers-graphql'
+    db_epic_table = os.environ.get('SQLALCHEMY_EPIC_TABLE')
+    db_jira_table = os.environ.get('SQLALCHEMY_JIRA_TABLE')
+    db_schema = os.environ.get('SQLALCHEMY_DB_SCHEMA')
 
     # Create Engine
     engine = create_engine(URI, echo=True)
@@ -22,34 +22,35 @@ class DatabaseImport:
     @classmethod
     def merge_epic_metadata(cls, jira_df):
         """Merge epic table."""
-        epics_df = pd.read_sql_table(cls.epic_table, cls.engine, schema='hackers$prod')
-        print(cls.epic_table)
-        jira_df = pd.merge(jira_df, epics_df[['epic_link', 'epic_name', 'epic_color']], how='left', on='epic_link', copy=False)
+        epics_df = pd.read_sql_table(cls.db_epic_table, cls.engine, schema=cls.db_schema)
+        jira_df = pd.merge(jira_df, epics_df[['epic_link', 'epic_name', 'epic_color']],
+                           how='left',
+                           on='epic_link',
+                           copy=False)
         return jira_df
 
     @classmethod
     def upload_dataframe_to_database(cls, jira_df):
         """Upload JIRA df to Postgres."""
         jira_df = cls.merge_epic_metadata(jira_df)
-        jira_df.drop(0, axis=1, inplace=True)
-        jira_df.to_sql(cls.jira_table, cls.engine, if_exists='replace', schema='hackers$prod', dtype={"id": Integer,
-                                                                                                      "assignee": Text,
-                                                                                                      "assignee_url": Text,
-                                                                                                      "epic_link": Text,
-                                                                                                      "issuetype": Text,
-                                                                                                      "issuetype_icon": Text,
-                                                                                                      "key": Text,
-                                                                                                      "priority": Text,
-                                                                                                      "priority_rank": Text,
-                                                                                                      "priority_url": Text,
-                                                                                                      "project": Text,
-                                                                                                      "status": Text,
-                                                                                                      "summary": Text,
-                                                                                                      "updated": Text,
-                                                                                                      "updatedAt": TIMESTAMP,
-                                                                                                      "createdAt": TIMESTAMP,
-                                                                                                      "epic_color": Text,
-                                                                                                      "epic_name": Text
-                                                                                                      })
-        success_message = 'Successfully uploaded' + str(jira_df.count) + ' rows to ' + cls.db_name
+        jira_df.to_sql(cls.db_jira_table, cls.engine, if_exists='replace', schema=cls.db_schema, dtype={"id": Integer,
+                                                                                                        "assignee": Text,
+                                                                                                        "assignee_url": Text,
+                                                                                                        "epic_link": Text,
+                                                                                                        "issuetype": Text,
+                                                                                                        "issuetype_icon": Text,
+                                                                                                        "key": Text,
+                                                                                                        "priority": Text,
+                                                                                                        "priority_rank": Text,
+                                                                                                        "priority_url": Text,
+                                                                                                        "project": Text,
+                                                                                                        "status": Text,
+                                                                                                        "summary": Text,
+                                                                                                        "updated": Text,
+                                                                                                        "updatedAt": TIMESTAMP,
+                                                                                                        "createdAt": TIMESTAMP,
+                                                                                                        "epic_color": Text,
+                                                                                                        "epic_name": Text
+                                                                                                        })
+        success_message = 'Successfully uploaded' + str(jira_df.count) + ' rows to ' + cls.db_jira_table
         return success_message
